cc6f4686f7c74d3b50b7ed1f2109778482d3cc04
quentin damois <quentin.damois@efrei.net>
2023-09-04 16:43:43 -0400

    Addedd prevention in order to get credible temperature

--- exercice_temperature_class/src/Main.java
+++ exercice_temperature_class/src/Main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a temperature in Fahrenheit : ");
+        System.out.println("Enter a temperature in Fahrenheit: ");
         Temperature t = new Temperature(input.nextDouble());
-        System.out.println("Temperature in Celsius : " + t.getCelsius());
-        System.out.println("Temperature in Kelvin : " + t .getKelvin());
+        while (t.getFahrenheit() < -459.67) {
+            System.out.printf("ERROR: %.2f is below the absolute zero which is - 459.67.\nEnter a temperature in Fahrenheit:\n", t.getFahrenheit());
+            t = new Temperature(input.nextDouble());
+        }
+        System.out.println("Temperature in Celsius: " + t.getCelsius());
+        System.out.println("Temperature in Kelvin: " + t .getKelvin());
     }
 }
