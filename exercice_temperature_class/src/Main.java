7154a4f9aef5cc4fe077056d3c35574736185ad2
Dylan Quellet <dylan.quellet@efrei.net>
2023-09-04 16:33:31 -0400

    Correction ponctuation

--- exercice_temperature_class/src/Main.java
+++ exercice_temperature_class/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a temperature in Fahrenheit : ");
+        System.out.println("Enter a temperature in Fahrenheit: ");
         Temperature t = new Temperature(input.nextDouble());
-        System.out.println("Temperature in Celsius : " + t.getCelsius());
-        System.out.println("Temperature in Kelvin : " + t .getKelvin());
+        System.out.println("Temperature in Celsius: " + t.getCelsius());
+        System.out.println("Temperature in Kelvin: " + t .getKelvin());
     }
 }
